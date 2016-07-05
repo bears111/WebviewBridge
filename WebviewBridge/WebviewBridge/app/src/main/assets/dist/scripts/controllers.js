93b1128eaf851840b03f5f1e7127f0f46a6d6b30
bears111 <703095394@qq.com>
2016-07-05 14:31:31 +0800

    哈哈哈

--- WebviewBridge/WebviewBridge/app/src/main/assets/dist/scripts/controllers.js
+++ WebviewBridge/WebviewBridge/app/src/main/assets/dist/scripts/controllers.js
@@ -20,7 +20,6 @@ $scope.showPopup1=function(){
    }
    );
 }
-
 }])
 .controller('LoginCtrl',['$scope', '$ionicPopup', '$timeout', '$state',  '$data', function($scope, $ionicPopup, $timeout, $state, $data) {
         $scope.formUser = {};
@@ -34,105 +33,7 @@ $scope.showPopup1=function(){
            myPopup.close();
            $state.go("dash");
            },1000)
-
         };
-
-        $scope.showErrorMesPopup = function(title) {
-            var myPopup = $ionicPopup.show({
-                title: '<b>'+title+'</b>'
-            });
-            $timeout(function() {
-                myPopup.close(); // 2秒后关闭
-            }, 1000);
-        };
-
-        $scope.showSuccessMesPopup = function(title) {
-            var myPopup = $ionicPopup.show({
-                title: '<b>'+title+'</b>',
-                template: '<p style="text-align: center"><ion-spinner icon="android" class="spinner-positive"></ion-spinner></p>'
-            });
-            $timeout(function() {
-                myPopup.close(); // 2秒后关闭
-                $state.go("tab.main");
-            }, 2000);
-        };
-    }])
- .controller('ModifyPasswordCtrl',['$scope', '$ionicPopup', '$timeout', '$state',  '$data', function($scope, $ionicPopup, $timeout, $state, $data) {
-        $scope.formUser = {};
-
-        //执行用户登录操作
-        $scope.doModify = function(){
-             $data.modifyPassword("user", this.formUser).success(function(data){
-                 if(data == null){
-                     $scope.showErrorMesPopup("不存在该用户");
-                 }else{
-                     $scope.showSuccessMesPopup("正在修改请稍后");
-                     $state.go("login");
-                 }
-          });
-        };
-
-        $scope.showErrorMesPopup = function(title) {
-            var myPopup = $ionicPopup.show({
-                title: '<b>'+title+'</b>'
-            });
-            $timeout(function() {
-                myPopup.close(); // 2秒后关闭
-            }, 1000);
-        };
-
-        $scope.showSuccessMesPopup = function(title) {
-            var myPopup = $ionicPopup.show({
-                title: '<b>'+title+'</b>',
-                template: '<p style="text-align: center"><ion-spinner icon="android" class="spinner-positive"></ion-spinner></p>'
-            });
-            $timeout(function() {
-                myPopup.close(); // 2秒后关闭
-                $state.go("tab.main");
-            }, 2000);
-            $timeout(function(){
-
-            },3000)
-        };
-    }])  
-
-.controller('RegisterCtrl',['$scope','$ionicPopup', '$timeout', '$state',  '$data', function($scope, $ionicPopup, $timeout, $state , $data) {
-        $scope.formUser = {};
-        $scope.doRegister = function(){
-            console.log($scope.formUser);
-            $data.register("user",$scope.formUser).success(function(data){
-                console.log(data,"true OR false");
-                if(data != null){
-                    $scope.showSuccessMesPopup("正在注册，请稍后");
-                    $timeout(function() {
-                        $scope.showErrorMesPopup("注册成功！");
-                        $state.go("login");
-                    }, 2000);
-                }else{
-                    $scope.showErrorMesPopup("用户名已被注册，请更换！");
-                }
-            });
-        };
-
-
-        $scope.showErrorMesPopup = function(title) {
-            var myPopup = $ionicPopup.show({
-                title: '<b>'+title+'</b>'
-            });
-            $timeout(function() {
-                myPopup.close(); // 2秒后关闭
-            }, 1000);
-        };
-
-
-        //  验证成功后提示框
-        $scope.showSuccessMesPopup = function(title) {
-            var myPopup = $ionicPopup.show({
-                title: '<b>'+title+'</b>',
-                template: '<p style="text-align: center"><ion-spinner icon="android"></ion-spinner></p>'
-            });
-            $timeout(function() {
-                myPopup.close(); // 2秒后关闭
-            }, 2000);
-        };
-    }])
+}])
+.controller('ModifyPasswordCtrl',['$scope', '$ionicPopup', '$timeout', '$state',  '$data', function($scope, $ionicPopup, $timeout, $state, $data) {}])
+.controller('RegisterCtrl',['$scope','$ionicPopup', '$timeout', '$state',  '$data', function($scope, $ionicPopup, $timeout, $state , $data) {}])
